8e0aacb9cee6ba95fd05c50a9191033a149d2e51
koeuk-Developer <koeukkos@gmail.com>
2024-06-20 15:01:47 +0700

    pust source code Vertual Company1

---
+++ admin_router.php
@@ -0,0 +1,35 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$page = "";
+$routes = [
+    '/admin_home' => 'controllers/admin/admin.controller.php',
+
+    // This is for Courses page
+    '/courses_as_admin' => 'controllers/admin/courses/course.controller.php',
+    '/courseEdit' => 'controllers/admin/courses/course_edit.controller.php',
+    '/createCourse' => 'controllers/admin/courses/create_course.controller.php',
+    '/viewCourse' => 'controllers/admin/courses/course.controller.php',
+    '/updateCourse' => 'controllers/admin/courses/update_course.controller.php',
+
+    //This is for category page
+    '/categories' => 'controllers/admin/category/category.controller.php',
+    '/editCategory' => 'controllers/admin/category/category_edit.controller.php',
+    '/deleteCategory' => 'controllers/admin/category/category_delete.controller.php',
+
+    '/list_trainer' => 'controllers/admin/trainer/trainer.controller.php',
+    '/add_trainer' => 'controllers/admin/trainer/add.controller.php',
+    '/list_student'=>'controllers/admin/students/student_controller.php',
+
+];
+
+if (array_key_exists($uri, $routes)) {
+    $page = $routes[$uri];
+}
+ else {
+   http_response_code(404);
+   $page = 'views/errors/404.php';
+}
+require "layouts/admin/header.php";
+require "layouts/admin/navbar.php";
+require $page;
+require "layouts/admin/footer.php";
---
+++ authentication_router.php
@@ -0,0 +1,49 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$page = "";
+$routes = [
+
+    '/admin_signin' => 'controllers/admin/signin/login.controller.php',
+    '/admin_access' => 'controllers/admin/signin/access.controller.php',
+    '/admin_password' => 'controllers/admin/passwords/passwrod.controller.php',
+    '/admin_password_comfirm' => 'controllers/admin/passwords/getnew.controller.php',
+    
+    '/pro' => '/Users/koeuk/Downloads/g5-e-learning 11/controllers/students/pro.controller.php',
+    '/student_password' => 'controllers/students/passwords/passwrod.controller.php',
+    '/student_password_comfirm' => 'controllers/students/passwords/getnew.controller.php',
+
+    '/signin' => 'controllers/students/signin/signin.controller.php',
+    '/signup' => 'controllers/students/signup/signup.controller.php',
+    '/student' => 'controllers/students/home.controller.php',
+    '/student_profile' => 'controllers/students/profile.controller.php',
+    '/edit' => 'controllers/students/edit/edit.controller.php',
+    '/get_edit' => 'controllers/students/edit/get.edit.controller.php',
+    '/create_student' => 'controllers/students/signup/create.user.controller.php',
+    '/access' => 'controllers/students/signin/access.controller.php',
+    '/course' => 'controllers/courses/course.controller.php',
+    '/trainerLogin' => 'controllers/trainer/trainerLogin.controller.php',
+    '/trainerAccess' => 'controllers/trainer/trainerLoginProcess.controller.php',
+    '/formChangeNumber' => 'controllers/students/forgetPassword/formChangePassword.controller.php',
+    '/blog_learning' => 'controllers/courses/blog_learning.controller.php',
+    '/trainer_detail' => 'controllers/trainers/trainer.controller.php',
+    '/orders' => 'controllers/students/payments/order.controller.php',
+
+    '/trainer' => 'controllers/trainers/signin/login.controller.php',
+    '/trainer_access' => 'controllers/trainers/signin/access.controller.php',
+    '/trainer_home' => 'controllers/trainers/hom.controller.php',
+    '/trainer_manage' => 'controllers/trainers/trainer.controller.php',
+    '/trainer_edits' => 'controllers/trainers/edit.controller.php',
+    '/trainer_password' => 'controllers/trainers/passwords/passwrod.controller.php',
+    '/trainer_passwrod_comfirm' => 'controllers/trainers/passwords/getnew.controller.php',
+    '/detail' => 'controllers/admin/students/student_detail.controller.php',
+
+
+];
+
+if (array_key_exists($uri, $routes)) {
+    $page = $routes[$uri];
+} else {
+   http_response_code(404);
+   $page = 'views/errors/404.php';
+}
+require $page;
